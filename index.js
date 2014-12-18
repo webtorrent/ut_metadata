cc6c738538de6caa67192f1b521b019154a00d51
Feross Aboukhadijeh <feross@feross.org>
2014-12-17 22:17:08 -0800

    update comment for clarity

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ module.exports = function (metadata) {
     }
   }
 
-  // For IE support. Since function.name does not work in IE.
+  // Name of the bittorrent-protocol extension
   ut_metadata.prototype.name = 'ut_metadata'
 
   ut_metadata.prototype.onHandshake = function (infoHash, peerId, extensions) {
